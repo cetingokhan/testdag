26bdaa03287db6e16c981135ad27b91ce96d0d9f
Gökhan Çetin <44244086+cetingokhan@users.noreply.github.com>
2025-03-11 20:52:57 +0000

    ff

--- testdag.py
+++ testdag.py
@@ -6,10 +6,10 @@ from airflow.providers.cncf.kubernetes.operators.job import KubernetesPodOperato
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2023, 1, 1),
-    'retries': 1,
+    'retries': 1
 }
 
-with DAG('sample_100_tasks_dag', default_args=default_args, schedule_interval='@daily', concurrency=100) as dag:
+with DAG('sample_100_tasks_dag', default_args=default_args, schedule_interval='@daily', concurrency=100, catchup=False) as dag:
     
     start = DummyOperator(task_id='start')
     

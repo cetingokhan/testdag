a0eef9d935288bd2ee52de325ef7d274807feb77
Gökhan Çetin <44244086+cetingokhan@users.noreply.github.com>
2025-03-11 19:24:10 +0000

    fisrt test

---
+++ testdag.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import 
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+}
+
+with DAG('sample_100_tasks_dag', default_args=default_args, schedule_interval='@daily', concurrency=100) as dag:
+    
+    start = DummyOperator(task_id='start')
+    
+    tasks = []
+    for i in range(1, 101):
+        task = KubernetesPodOperator(
+                task_id=f'task_{i}',
+                name='run_python_command',
+                namespace='default',
+                image='python:3.8-slim',
+                cmds=["python", "-c"],
+                arguments=["print('Hello from the Python base image!')"],
+                is_delete_operator_pod=True,
+                get_logs=True,
+            )
+
+
+        #task = DummyOperator(task_id=f'task_{i}')
+        tasks.append(task)
+    
+    end = DummyOperator(task_id='end')
+    
+    start >> tasks >> end
